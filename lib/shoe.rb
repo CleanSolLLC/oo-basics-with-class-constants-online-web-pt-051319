d3d0485e1365d1eb78574330665d76e8d995c210
Mark May <mamark90@gmail.com>
2019-06-02 15:57:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -9,11 +9,11 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS << brand
-    binding.pry
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  binding.pry
 end
